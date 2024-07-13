ee30d09d5cf3747533a25812ba7e0aabb5b1dc5b
GoshaLebowski <sirinkinegor@gmail.com>
2024-07-13 15:21:19 +0500

    NestJS Rest API с пагинацией и CRUD операциями. Транзакции.

--- server/src/transaction/transaction.controller.ts
+++ server/src/transaction/transaction.controller.ts
@@ -1,34 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UsePipes,
+    ValidationPipe,
+    UseGuards,
+    Req, Query
+} from '@nestjs/common';
+import {TransactionService} from './transaction.service';
+import {CreateTransactionDto} from './dto/create-transaction.dto';
+import {UpdateTransactionDto} from './dto/update-transaction.dto';
+import {JwtAuthGuard} from "../auth/guards/jwt-auth.guard";
 
 @Controller('transaction')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+    constructor(private readonly transactionService: TransactionService) {
+    }
 
-  @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
-  }
+    @Post()
+    @UsePipes(new ValidationPipe())
+    @UseGuards(JwtAuthGuard)
+    async create(@Body() createTransactionDto: CreateTransactionDto, @Req() req: any) {
+        return this.transactionService.create(createTransactionDto, req.user.id);
+    }
+    
+    @Get('pagination')
+    @UseGuards(JwtAuthGuard)
+    findAllWithPagination(
+        @Req() req: any,
+        @Query('page') page: number,
+        @Query('limit') limit: number
+    ) {
+        return this.transactionService.findAllWithPagination(req.user.id, +page, +limit);
+    }
 
-  @Get()
-  findAll() {
-    return this.transactionService.findAll();
-  }
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    findAll(@Req() req: any) {
+        return this.transactionService.findAll(req.user.id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.transactionService.findOne(id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
+        return this.transactionService.update(id, updateTransactionDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.transactionService.remove(id);
+    }
+
 }
--- server/src/transaction/transaction.service.ts
+++ server/src/transaction/transaction.service.ts
@@ -1,26 +1,92 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
+import {CreateTransactionDto} from './dto/create-transaction.dto';
+import {UpdateTransactionDto} from './dto/update-transaction.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Transaction} from "./entities/transaction.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
-  }
+    constructor(
+        @InjectRepository(Transaction)
+        private readonly transactionRepository: Repository<Transaction>,
+    ) {
+    }
 
-  findAll() {
-    return `This action returns all transaction`;
-  }
+    async create(createTransactionDto: CreateTransactionDto, id: string) {
+        const newTransaction = {
+            title: createTransactionDto.title,
+            amount: createTransactionDto.amount,
+            type: createTransactionDto.type,
+            category: { id: createTransactionDto.category.id }, // Скорректируйте, чтобы получить доступ к id свойства Category
+            user: { id },
+        }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
+        if (!newTransaction) throw new BadRequestException('Something went wrong...')
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+        return await this.transactionRepository.save(newTransaction);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
-  }
+    async findAll(id: string) {
+        const transaction = this.transactionRepository.find({
+            where: {
+                user: {id},
+            },
+            order: {
+                createdAt: 'DESC',
+            },
+        });
+        if (!transaction) throw new NotFoundException('Something went wrong...')
+        return transaction
+    }
+
+    async findOne(id: string) {
+        const transaction = await this.transactionRepository.findOne({
+            where: {
+                id,
+            },
+            relations: {
+                user: true,
+                category: true
+            }
+        })
+        if (!transaction) throw new NotFoundException('Something went wrong...')
+        if (transaction.user) {delete transaction.user.password}
+        return transaction;
+    }
+
+    async update(id: string, updateTransactionDto: UpdateTransactionDto) {
+        const transaction = this.transactionRepository.findOne({
+            where: {id},
+        })
+
+        if (!transaction) {
+            throw new NotFoundException('Transaction not found!')
+        }
+
+        return await this.transactionRepository.update(id, updateTransactionDto);
+    }
+
+    async remove(id: string) {
+        const transaction = await this.transactionRepository.findOne({
+            where: {id},
+        })
+
+        if (!transaction) throw new NotFoundException('Transaction not found!')
+
+        return await this.transactionRepository.delete(id);
+    }
+
+    async findAllWithPagination(id: string, page: number, limit: number) {
+        return await this.transactionRepository.find({
+            where: {
+                user: {id}
+            },
+            order: {
+                createdAt: 'DESC',
+            },
+            take: limit,
+            skip: (page - 1) * limit
+        })
+    }
 }
