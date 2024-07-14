682eb727f92b74472e1e2140692b738e13857859
GoshaLebowski <sirinkinegor@gmail.com>
2024-07-14 21:47:13 +0500

    ReactJS Routing. Делаем навигацию по приложению. Layout

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,11 +1,26 @@
-import {FC} from "react";
+import { FC } from 'react'
+import notFound from '../assets/notFound.png'
+import { Link } from 'react-router-dom'
 
 const ErrorPage: FC = () => {
-    return (
-        <div>
-            ErrorPage
-        </div>
-    );
-};
+	return (
+		<div className={
+			' min-h-screen' +
+			' bg-slate-900' +
+			' font-roboto' +
+			' text-white' +
+			' flex' +
+			' justify-center' +
+			' items-center' +
+			' flex-col' +
+			' gap-10'
+		}>
+			<img src={notFound} alt="img" />
+			<Link to={'/'} className={'bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600'}>
+				Back
+			</Link>
+		</div>
+	)
+}
 
-export default ErrorPage;
+export default ErrorPage
