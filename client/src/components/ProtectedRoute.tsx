a93013d1754d59f419b0864ec6dd83c91e725c04
GoshaLebowski <sirinkinegor@gmail.com>
2024-07-15 16:38:57 +0500

    Приватные роуты ReactJS.Protected Route. Fullstack NestJS ReactJS

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,23 @@
+import { FC, JSX } from 'react'
+import { useAuth } from '../hooks/useAuth.ts'
+import protectedImg from '../assets/protected.png'
+
+interface Props {
+	children: JSX.Element
+}
+
+export const ProtectedRoute: FC<Props> = ({ children }) => {
+	const isAuth = useAuth()
+
+	return (
+		<>
+			{
+				isAuth ? children : <div className={'flex flex-col justify-center items-center mt-20 gap-10'}>
+					<h1 className={'text-2xl'}>To view this page you must be logged in</h1>
+
+					<img className={'w-1/3'} src={protectedImg} alt={'img'} />
+				</div>
+			}
+		</>
+	)
+}
--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,33 +1,40 @@
-import {createBrowserRouter} from "react-router-dom";
-import Layout from "../pages/Layout.tsx";
-import ErrorPage from "../pages/ErrorPage.tsx";
-import Home from "../pages/Home.tsx";
-import Transactions from "../pages/Transactions.tsx";
-import Categories from "../pages/Categories.tsx";
-import Auth from "../pages/Auth.tsx";
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from '../pages/Layout.tsx'
+import ErrorPage from '../pages/ErrorPage.tsx'
+import Home from '../pages/Home.tsx'
+import Transactions from '../pages/Transactions.tsx'
+import Categories from '../pages/Categories.tsx'
+import Auth from '../pages/Auth.tsx'
+import { ProtectedRoute } from '../components/ProtectedRoute.tsx'
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout/>,
-        errorElement: <ErrorPage/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>
-            },
-            {
-                path: 'transactions',
-                element: <Transactions/>,
-            },
-            {
-                path: 'categories',
-                element: <Categories/>
-            },
-            {
-                path: 'auth',
-                element: <Auth/>
-            }
-        ]
-    }
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				index: true,
+				element: <Home />
+			},
+			{
+				path: 'transactions',
+				element:
+					<ProtectedRoute>
+						<Transactions/>
+					</ProtectedRoute>
+			},
+			{
+				path: 'categories',
+				element:
+					<ProtectedRoute>
+						<Categories/>
+					</ProtectedRoute>
+			},
+			{
+				path: 'auth',
+				element: <Auth />
+			}
+		]
+	}
 ])
